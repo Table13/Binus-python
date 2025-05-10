89002741790df88828236d6fcbdb4de1be0816ea
Table13 <88639939+Table13@users.noreply.github.com>
2025-05-10 15:53:12 +0700

    Create LuasLingkaran.py

---
+++ pertemuan1/LuasLingkaran.py
@@ -0,0 +1,20 @@
+"""
+JUDUL
+Menghitung Luas Lingkaran
+//////
+Deklarasi:
+diameter, pi, luas : interger
+//////
+Deskipsi:
+start
+Masukan(diameter)
+
+Luas = pi * int_diameter
+
+OUTPUT LuasLingkaran
+end
+"""
+int_diameter= int(input("masukan Diameter: "))
+pi = 3.14
+luas = pi * int_diameter
+print(luas)
