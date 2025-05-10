d98b2aa66bd0665a3e3ba353033773f585e36ce6
Table13 <88639939+Table13@users.noreply.github.com>
2025-05-10 15:40:38 +0700

    Create LuasLapanganBola.py

---
+++ pertemuan1/LuasLapanganBola.py
@@ -0,0 +1,19 @@
+"""
+JUDUL
+Menghitung luas lapangan bola
+////////
+deklarasi:
+panjang, lebar, luas : interger
+////////
+deksripsi:
+INPUT panjang dan lebar
+
+luas = panjang * lebar
+
+OUTPUT luas
+"""
+int_Panjang= int(input("masukan panjang: "))
+int_Lebar = int(input("masukan Lebar: "))
+
+luas = int_Panjang * int_Lebar
+print(luas)
