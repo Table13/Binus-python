ca0ad017c4332581973d1b57de5918a34ee03732
Table13 <88639939+Table13@users.noreply.github.com>
2025-05-11 08:37:03 +0700

    Create CekSuhu.py

---
+++ pertemuan1/CekSuhu.py
@@ -0,0 +1,32 @@
+"""
+JUDUL
+Konvensi suhu
+///////
+Deklarasi:
+SUHU, farenhaiet, celsius, kelvin
+///////
+Deksripsi:
+START
+Input(suhu awal)
+Input(jenisSuhuAwal dan jenisSuhuAkhir)
+
+if JenisSuhuAkhir is Celsius
+    if JenisSuhuAwal is fahrenheit
+        print(SuhuCelsius = 5/9 * (suhu awal - 32))
+    if JenisSuhuAwal is kelvin
+        print(SuhuCelsius = suhu awal - 273)
+        
+if jenisSuhuAkhir is fahrenheit
+    if JenisSuhuAwal is Celcius
+        print(SuhuFarenheit = 9/5 * suhu awal + 32)
+    if jenisSuhuAwal is kelvin
+        print(SuhuFarenhait = 9/5 * (suhu awal - 273) + 32)
+        
+if jenisSuhuAkhir is Kelvin
+    if JenisSuhuAwal is Fahrenhaeit
+        print(SuhuKelvin = 5/9 (suhu awal - 32) + 273
+    if JenisSuhuAwal is Celsius
+        print(suhuKelvin = C + 273)
+
+END
+"""
