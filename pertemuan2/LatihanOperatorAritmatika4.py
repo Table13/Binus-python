0526b0c3f3addbcf78f54a7ec390ad7ec0d722f1
Table13 <88639939+Table13@users.noreply.github.com>
2025-05-17 14:01:23 +0700

    Create LatihanOperatorAritmatika4.py

---
+++ pertemuan2/LatihanOperatorAritmatika4.py
@@ -0,0 +1,6 @@
+int_nilaiA = int(input("masukan Nilai A: ")
+int_nilaiB = int(input("masukan Nilai B: ")
+int_nilaiC = int(input("masukan Nilai C: ")
+
+Diskriminan = int_nilaiB ** 2 - 4 * int_nilaiA * int_nilaiC
+print("Nilai diskriminan adalah", Diskriminan)
