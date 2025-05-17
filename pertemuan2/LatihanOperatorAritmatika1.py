0168cf13216cbd8b9d7805c1065ecd92d4be41f4
Table13 <88639939+Table13@users.noreply.github.com>
2025-05-17 13:51:35 +0700

    Create LatihanOperatorAritmatika1.py

---
+++ pertemuan2/LatihanOperatorAritmatika1.py
@@ -0,0 +1,6 @@
+int_l = int(input("masukan lebar: "))
+int_t = int(input("masukan tinggi: "))
+int_p = int(input("masukan panjang: "))
+
+volume = int_l * int_t * int_p
+print("panjang volume balok adalah",volume,"m3")
