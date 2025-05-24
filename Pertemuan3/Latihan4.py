b65836564ce3762028cd621e86d3566f87ff1c32
Table13 <88639939+Table13@users.noreply.github.com>
2025-05-24 15:46:39 +0700

    Update Latihan4.py

--- Pertemuan3/Latihan4.py
+++ Pertemuan3/Latihan4.py
@@ -1,38 +1,28 @@
-print("""
-Pilih jenis konvensi yang anda mau
-1.Celcius ke Fahrenheit
-2.Celcius ke Kelvin
-3.Fahrenheit ke Celsius
-4.Fahrenheit ke Kelvin
-5.Kelvin ke Celcius
-6.Kelvin ke Fahrenheit
-""")
+A = int(input("SISI A : "))
+B = int(input("SISI B : "))
+C = int(input("SISI C : "))
 
-jenisKonvensi = int(input("masukan konvensi berdasarkan nomernya: "))
-
-if jenisKonvensi == 1:
-    Suhu = int(input("masukan Suhu yang ingin di ubah: "))
-    F = 9/5 * Suhu + 32
-    print("Suhu yang di ingin di ubah adalah Celsius ke Fahrenheit.",Suhu,"Celsius menjadi",F,"Fahrenheit")
-elif jenisKonvensi == 2:
-    Suhu = int(input("masukan Suhu yang ingin di ubah: "))
-    K = Suhu + 273
-    print("Suhu yang di ingin di ubah adalah Celsius ke Kelvin.",Suhu,"Celsius menjadi",K,"Kelvin")
-elif jenisKonvensi == 3:
-    Suhu = int(input("masukan Suhu yang ingin di ubah: "))
-    C = 5/9 * Suhu - 32
-    print("Suhu yang di ingin di ubah adalah Fahrenheit ke Celsius.",Suhu,"Fahrenheit menjadi",C,"Celsius")
-elif jenisKonvensi == 4:
-    Suhu = int(input("masukan Suhu yang ingin di ubah: "))
-    K = 5/9 * (Suhu-32) + 273
-    print("Suhu yang di ingin di ubah adalah Fahrenheit ke Kelvin.",Suhu,"Fahrenheit menjadi",K,"Kelvin")
-elif jenisKonvensi == 5:
-    Suhu = int(input("masukan Suhu yang ingin di ubah: "))
-    C = Suhu - 273
-    print("Suhu yang di ingin di ubah adalah Kelvin ke Celsius.",Suhu,"Kelvin menjadi",C,"Celsius")
-elif jenisKonvensi == 6:
-    Suhu = int(input("masukan Suhu yang ingin di ubah: "))
-    F = 9/5 * (Suhu-273) + 32
-    print("Suhu yang di ingin di ubah adalah Kelvin ke Fahrenheit.",Suhu,"Kelvin menjadi",F,"Fahrenheit")
+if ((A + B > C) and (C + B > A) and (A + C > B)):
+    if ((A == B) and (A == C) and (C == B)):
+        print("INI ADALAH SEGITIGA SAMA SISI")
+        if A**2 + B **2 < C**2:
+            print("SEGITIGA LANCIP")
+        else:
+            print("SEGITIGA TUMPUL")
+    elif ((A == B) or (A == C) or (C == B)):
+        print("INI ADALAH SEGITIGA SAMA KAKI")
+        if A**2 + B **2 < C**2:
+            print("SEGITIGA LANCIP")
+        else:
+            print("SEGITIGA TUMPUL")
+    elif A**2 + B **2 == C**2:
+        print("INI ADALAH SEGITIGA SIKU SIKU")
+    else:
+        print("INI ADALAH SEGITIGA SEMBARANG")
+        if A**2 + B **2 < C**2:
+            print("SEGITIGA LANCIP")
+        else:
+            print("SEGITIGA TUMPUL")
 else:
-    print("MOHON PILIH JENIS NOMOR KONVESI YANG TERTERA!!!")
+    print("BUKAN SEGITIGA")
+    
