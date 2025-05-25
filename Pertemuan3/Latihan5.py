93492160000501d3efb82346ea89399a0c94413f
Table13 <88639939+Table13@users.noreply.github.com>
2025-05-26 06:39:26 +0700

    Update Latihan5.py

--- Pertemuan3/Latihan5.py
+++ Pertemuan3/Latihan5.py
@@ -3,10 +3,13 @@ b = int(input("masukan nilai B: "))
 c = int(input("masukan nilai C: "))
 
 d = b**2 - 4*a*c
-print(d)
+
+x1 = -b + (d ** 0.5) / 2 * a
+x2 = -b - (d ** 0.5) / 2 * a
 
 if d > 0: #nilai akar asli
-    print("nilai diskriminan",d,"artinya kedua akar adalah bilangan asli yaitu X1 =",x1,". X2 ="x2)
-elif d = 0: #memiliki akar kembar
-     print("nilai diskriminan",d,"artinya akar adalah angka kemabr yaitu", x1)
+    print("nilai diskriminan",d,"artinya kedua akar adalah bilangan asli yaitu X1 =",x1,". X2 =",x2)
+elif d == 0: #memiliki akar kembar
+    print("nilai diskriminan",d,"artinya akar adalah angka kemabr yaitu", x1)
 elif d < 0: #memiliki angka imajinier
+    print("nilai diskriminan",d,"artinya kedua akar memiliki angka imjainer yaitu X1 =",x1,". X2 =",x2)
